75e433ec759b52ee2c9f7abf45eecad0e5a25cb1
Nikel Arteta <nikelarteta@Thanhs-Mac-mini.local>
2017-04-07 17:51:40 +0700

    - Update.

--- Classes/MathUtils.cpp
+++ Classes/MathUtils.cpp
@@ -11,8 +11,9 @@
 
 NS_SS_BEGIN
 
-bool MathUtils::ccw(cocos2d::Vec2 A, cocos2d::Vec2 B, cocos2d::Vec2 C) {
-    return (C.y-A.y)*(B.x-A.x) > (B.y-A.y)*(C.x-A.x);
+bool MathUtils::ccw(const cocos2d::Vec2& A, const cocos2d::Vec2& B,
+                    const cocos2d::Vec2& C) {
+    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
 }
 
 /* Changes from header:
@@ -21,26 +22,26 @@ bool MathUtils::ccw(cocos2d::Vec2 A, cocos2d::Vec2 B, cocos2d::Vec2 C) {
     - C is A2
     - D is B2
  */
-bool MathUtils::linesInterect(cocos2d::Vec2 A, cocos2d::Vec2 B, cocos2d::Vec2 C, cocos2d::Vec2 D) {
-    return ccw(A,C,D) != ccw(B,C,D) && ccw(A,B,C) != ccw(A,B,D);
+bool MathUtils::linesInterect(const cocos2d::Vec2& A, const cocos2d::Vec2& B,
+                              const cocos2d::Vec2& C, const cocos2d::Vec2& D) {
+    return ccw(A, C, D) != ccw(B, C, D) && ccw(A, B, C) != ccw(A, B, D);
 }
 
-bool MathUtils::polygonsIntersect(std::vector<cocos2d::Point>& poly1,
-                                  std::vector<cocos2d::Point>&  poly2)
-{
-    for (int i=0; i<poly1.size(); i++) {
-        int i1=i+1;
-        if (i==poly1.size()-1) {
-            i1=0;
+bool MathUtils::polygonsIntersect(const std::vector<cocos2d::Point>& poly1,
+                                  const std::vector<cocos2d::Point>& poly2) {
+    for (int i = 0; i < poly1.size(); i++) {
+        int i1 = i + 1;
+        if (i == poly1.size() - 1) {
+            i1 = 0;
         }
-        
+
         cocos2d::Vec2 a1 = poly1[i];
         cocos2d::Vec2 a2 = poly1[i1];
-        
-        for (int j=0; j<poly2.size(); j++) {
-            int j1=j+1;
-            if (j==poly2.size()-1) {
-                j1=0;
+
+        for (int j = 0; j < poly2.size(); j++) {
+            int j1 = j + 1;
+            if (j == poly2.size() - 1) {
+                j1 = 0;
             }
             cocos2d::Vec2 b1 = poly2[j];
             cocos2d::Vec2 b2 = poly2[j1];
--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -7,7 +7,7 @@ $(call import-add-path,$(COCOS2DX_ROOT)/external)
 $(call import-add-path,$(COCOS2DX_ROOT)/cocos)
 $(call import-add-path,$(COCOS2DX_ROOT)/cocos/editor-support/cocosbuilder)
 $(call import-add-path,$(COCOS2DX_ROOT)/extensions)
-$(call import-add-path,$(COCOS2DX_ROOT)/cocos/prebuilt-mk)
+#$(call import-add-path,$(COCOS2DX_ROOT)/cocos/prebuilt-mk)
 
 LOCAL_MODULE := sensparkgamefoundation_static
 
@@ -17,11 +17,14 @@ FILE_LIST := $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp)
 
 LOCAL_SRC_FILES := cpp/main.cpp
 LOCAL_SRC_FILES += $(FILE_LIST:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(LOCAL_PATH)/../../include/gameXtension.h
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../include/gameXtension.h
 					
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../include
 					
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_STATIC_LIBRARIES += cocosdenshion_static 
@@ -30,7 +33,7 @@ LOCAL_STATIC_LIBRARIES += cocosbuilder_static
 
 include $(BUILD_STATIC_LIBRARY)
 
-$(call import-module,.)
+#$(call import-module,.)
 #$(call import-module,extensions/prebuilt-mk)
 #$(call import-module,editor-support/cocosbuilder/prebuilt-mk)
 #$(call import-module,prebuilt-mk)
